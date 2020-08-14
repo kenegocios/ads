cc0e0b8058536d73384e1f9a33b50497ae4a9b7e
Jefferson Miranda <kenegocios.online@gmail.com>
2020-08-14 00:31:04 -0300

    Create cfg.js

---
+++ cfg.js
@@ -0,0 +1,17 @@
+// 1000 = 1 segundo
+// Controle de Eventos ao Carregar o Documento
+// Inicie eventos imediatamente
+var tIniciar = 0;
+// Finalize o Loader após segundos
+var tLoader = 3000;
+// Exibe o Modal após segundos 
+var tModal = 5000;
+var linkAff = "./ads/idplr"
+var linkAffClose = linkAff;
+var linkAffNext = linkAff;
+var linkAffOff = linkAff;
+var linkModalInativo = linkAff;
+var ModalInativo = 15000;
+var tmodalClose = 20000;
+var tmodalNext = 0;
+var tmodalOff = 20000;
