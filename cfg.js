d269b33bd22dd1da04b410c3415bfc664d355d85
Jefferson Miranda <kenegocios.online@gmail.com>
2020-08-17 23:52:05 -0300

    Update cfg.js

--- cfg.js
+++ cfg.js
@@ -1,17 +1,15 @@
-// 1000 = 1 segundo
 // Controle de Eventos ao Carregar o Documento
-// Inicie eventos imediatamente
-var tIniciar = 0;
-// Finalize o Loader após segundos
-var tLoader = 3000;
-// Exibe o Modal após segundos 
-var tModal = 5000;
-// var linkAff = "seu-link-aqui"
-var linkAffClose = linkAff;
-var linkAffNext = linkAff;
-var linkAffOff = linkAff;
-var linkModalInativo = linkAff;
-var ModalInativo = 15000;
-var tmodalClose = 20000;
-var tmodalNext = 0;
-var tmodalOff = 20000;
+// Time(1000 = 1 segundo)
+// Inicie eventos baseado no Time
+var tIniciar = 0; // para iniciar de imediato use '0'
+var tLoader = 2500; // Ficará ativo durante tempo definido
+var tModal = 15000; // Defina um Time para exibir o Modal após segundos
+var tmodalInativo = 30000; // Acionado quando inativo
+var tmodalClose = 20000; // Acionado ao clicar no <x> para fechar.
+var tmodalNext = 1000; // Acionado ao clicar em Avançar.
+var tmodalWindow = 30000; // Acionado quando clica fora do modal, dentro da página.
+//var meuLink = "Meu Link"
+var linkModalClose = linkAff; // Link do botão Fechar
+var linkModalNext = linkAff; // Link do botão Avançar
+var linkModalWindow = linkAff; // Link para quando clica fora do modal
+var linkModalInativo = linkAff; // Link para quando fica inativo por um tempo
